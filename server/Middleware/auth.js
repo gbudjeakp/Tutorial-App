be8ac97555286c3bf0f2a48b0c67335ba988f836
gbudjeakp <gbudjeakp@gmail.com>
2021-05-14 14:49:57 -0500

    adding more code

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -1,22 +1,22 @@
-// const jwt = require('jsonwebtoken');
-// const db = require("../Config/connection")
+const jwt = require('jsonwebtoken');
+const db = require("../Config/connection")
 
 
-// module.exports = async (req, res, next) => {
-//   try {
-//     const { token } = req.cookies;
-//     if (!token) throw new Error('Unauthenticated');
+module.exports = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) throw new Error('Unauthenticated');
 
-//     const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
 
-//     const user = await  "Add stuff"
+    const user = await  "Add stuff"
 
-//     if (!user) throw new Error('Unauthenticated');
+    if (!user) throw new Error('Unauthenticated');
 
-//     res.locals.user = user;
+    res.locals.user = user;
 
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({ error: 'Unauthenticated' });
-//   }
-// };
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthenticated' });
+  }
+};
--- server/Routes/User.js
+++ server/Routes/User.js
@@ -1,43 +1,46 @@
 const router = require("express").Router();
-const db = require("../Config/connection")
+const db = require("../Config/connection");
 // const jwt = require("jsonwebtoken");
 // const cookie = require("cookie");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
-router.route("/register").post((req, res) => {
-  const { fName, email, password } = req.body;
-  
-  const userData = {name: fName, email: email, password: password}
-  const addUser = 'INSERT INTO users SET ?'
-  const isExist = 'SELECT email from users WHERE email = ?'
-  
-   db.query(isExist, [email], (error, result)=>{
-     if(error){
-       console.log(error)
-     }
-     if(result.length > 0){
-       return res.status(500).json({message: "Email Already Exist"})
-     } else{
-      
-  db.query(addUser, userData, (err, result)=>{
-    if(err){
-      console.log(err)
-    } else{
-      res.status(200).json( {message: "User has been added"})
-    }
- });
-     }
-   })
-  // Here is login to print 
-  console.log(`${fName} has Registered to our app as ${email}`);
+router.route("/register").post(async (req, res) => {
+  try {
+    const { fName, email, password } = req.body;
+    let hashPassword = await bcrypt.hash(password, 8);
+    const userData = { name: fName, email: email, password: hashPassword };
+    const addUser = "INSERT INTO users SET ?";
+    const isExist = "SELECT email from users WHERE email = ?";
+
+    db.query(isExist, [email], (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+      if (result.length > 0) {
+        return res.status(409).json({ message: "Email Already Exist" });
+      } else {
+        db.query(addUser, userData, (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result);
+            return res.status(200).json({ message: "User Added" });
+          }
+        });
+      }
+    });
+  } catch (err) {
+   console.error(err)
+  }
 });
 
+//This route handles log-in validation
 router.route("/login").post((req, res) => {
   const { email, password } = req.body;
   console.log(`Here is your passowrd ${password} and your email ${email}`);
 });
 
-
+//This route send all the users to the front-end
 router.route("/allusers").get((req, res) => {
   console.log("Here are all the users");
   const allUsers = "SELECT * FROM  users ;";
